cd4b47b0a38ed70bda3e582f3e9c2de14f1e5ee2
Harry Taylor <harrytaylor170503@gmail.com>
2025-10-15 16:25:37 +0100

    initial commit

---
+++ src/Employee.java
@@ -0,0 +1,17 @@
+public class Employee {
+    private String name;
+    private int age;
+
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Getters
+    public String getName() {return name;}
+    public int getAge() {return age;}
+
+    // Setters
+    public void setName(String name) {this.name = name;}
+    public void setAge(int age) {this.age = age;}
+}
